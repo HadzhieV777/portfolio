c141e0decd9e68f3e0520bf0f9839864b0e71cea
HadzhieV777 <bdwhadjiev42@gmail.com>
2024-01-01 17:01:33 +0200

    Setup Hero

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section>
+        <div className='flex items-center justify-center'>
+            <div></div>
+        </div>
+    </section>
+  )
+}
