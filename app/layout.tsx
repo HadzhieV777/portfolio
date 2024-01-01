cc1b1d61a18ef78b8f01ffea2a3e0aeec9dec3da
HadzhieV777 <bdwhadjiev42@gmail.com>
2024-01-01 16:56:32 +0200

    Create navbar and implement some animations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Radoslav | Personal Portfolio",
   description:
-    "Radoslav is a seasoned Fullstack Software Developer. With a passion for crafting seamless, innovative solutions, bringing a unique blend of technical prowess and marketing acumen to the digital landscape.",
+    "Radoslav is a seasoned Full-Stack Software Developer. With a passion for crafting seamless, innovative solutions, bringing a unique blend of technical prowess and marketing acumen to the digital landscape.",
 };
 
 export default function RootLayout({
@@ -21,8 +22,9 @@ export default function RootLayout({
         className={`${inter.className} 
       bg-gray-50 text-gray-950`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        <Header />
         {children}
       </body>
     </html>
