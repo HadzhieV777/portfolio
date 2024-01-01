b591f2912751164479731f7f22b216b922b03aa0
HadzhieV777 <bdwhadjiev42@gmail.com>
2024-01-01 19:01:34 +0200

    Styling hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Divider from "@/components/Divider";
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="">
-    
+    <main className="flex flex-col items-center px-4">
+      <Hero />
+      {/* TODO Maybe Remove Divider */}
+      <Divider />
     </main>
-  )
+  );
 }
