2baccb7d529289f8fa29560594794eba018d5215
HadzhieV777 <bdwhadjiev42@gmail.com>
2024-01-08 22:15:45 +0200

    Add about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Divider from "@/components/Divider";
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Hero />
+      <About />
     </main>
   );
 }
